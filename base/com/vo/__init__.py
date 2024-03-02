9762d6472a73787251a7257efcea487ac790a373
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-03-02 22:42:29 +0530

    vo update

---
+++ base/com/vo/__init__.py
@@ -0,0 +1,3 @@
+from base.com.vo import user_vo
+from base.com.vo import restricted_vo
+from base.com.vo import helmet_vest_detection_vo
---
+++ base/com/vo/helmet_vest_detection_vo.py
@@ -0,0 +1,22 @@
+from base import db
+
+
+class HelmetVestDetectionVO(db.Model):
+    __tablename__ = 'helmet_vest_detection_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_name = db.Column(db.String(255), nullable=False)
+    safety_percentage = db.Column(db.Float, nullable=False)
+    unsafety_percentage = db.Column(db.Float, nullable=False)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'video_name': self.video_name,
+            'safety_percentage': self.safety_percentage,
+            'unsafety_percentage': self.unsafety_percentage
+        }
+
+
+with app.app_context():
+    db.create_all()
