1090826d63b327f4e0c9f785d97a1238f271f18c
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-04-14 00:31:13 +0530

    view added

--- base/com/controller/user_controller.py
+++ base/com/controller/user_controller.py
@@ -1,8 +1,14 @@
+from datetime import datetime
 from flask import render_template, redirect, request, session
 from flask_login import login_user, login_required, current_user, logout_user
 from base import app, login_manager
 from base.com.vo.user_vo import UserVO
 from base.com.dao.user_dao import UserDAO
+
+
+def remove_video_path_prefix(path):
+    path = path.split(r"\\")
+    pass
 
 
 @login_manager.user_loader
@@ -48,6 +54,32 @@ def login():
 @login_required
 def dashboard():
     return render_template('dashboard.html', user=current_user)
+
+
+@app.route('/view-detection')
+@login_required
+def view_detection():
+    user_dao_obj = UserDAO()
+    data = user_dao_obj.get_detection_records()
+
+    customized_data = []
+    for i in range(len(data)):
+        data_dict = {}
+
+        data_dict['sr_no'] = i+1
+        data_dict['detection_id'] = data[i].detection_id
+        data_dict['detection_stats'] = data[i].detection_stats
+        data_dict['input_file_path'] = data[i].input_file_path.split(
+            '\\')[-1].capitalize()
+        data_dict['output_file_path'] = data[i].output_file_path.split(
+            '\\')[-1].capitalize()
+        data_dict['detection_type'] = data[i].detection_type.capitalize()
+        data_dict['detection_source'] = data[i].detection_source.capitalize()
+        data_dict['detection_datetime'] = datetime.utcfromtimestamp(
+            data[i].detection_datetime).strftime('%Y-%m-%d %H:%M:%S')
+        customized_data.append(data_dict)
+
+    return render_template('user/view_detection.html', user=current_user, data=customized_data)
 
 
 @app.route("/logout")
--- base/com/dao/user_dao.py
+++ base/com/dao/user_dao.py
@@ -1,16 +1,18 @@
 from werkzeug.security import check_password_hash
+from flask_login import current_user
 from base import app, db
 from base.com.vo.user_vo import UserVO
+from base.com.vo.detection_vo import DetectionVO
 
 
 class UserDAO():
     def view_one_user(self, user_vo):
         user = UserVO.query.filter_by(
             login_username=user_vo.login_username).first()
-        
+
         if not user:
             return None
-        
+
         password = user_vo.login_password
         status = check_password_hash(user.login_password, password)
         if status:
@@ -20,3 +22,6 @@ class UserDAO():
     def save(self, user_vo):
         db.session.add(user_vo)
         db.session.commit()
+
+    def get_detection_records(self):
+        return DetectionVO.query.filter_by(is_deleted=False).filter_by(created_by=current_user.login_id).all()
